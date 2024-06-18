bab32547f0fab8e8207d14a462e3690bf41ff3cd
Nikita Kichaykin <unknol@outlook.com>
2024-06-19 02:56:09 +1000

    add event.type

--- script.js
+++ script.js
@@ -3,13 +3,12 @@ const testTextField2 = document.querySelector('#testTextField2');
 
 let clickCounter = 0;
 
-document.querySelector('#testTextField1').addEventListener('click',
-    function() {
+document.querySelector('#testTextField1').addEventListener('click', function() {
         clickCounter = clickCounter + 1; 
         this.textContent = 'Вы кликнули правильно! ' + clickCounter + ' РАЗ!';
 })
 
-document.querySelector('#testTextField2').addEventListener('click',
-    function() {
-        this.textContent = 'Вы кликнули не правильно!';
+document.querySelector('#testTextField2').addEventListener('click', function() {
+        this.textContent = 'Это событие:' + event.type;
+        console.log('x'+ event.clientX, 'у' + event.clientY);
 })
