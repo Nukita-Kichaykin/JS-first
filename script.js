aa28274a610bd4736b228b49a3a4327702895b54
Nikita Kichaykin <unknol@outlook.com>
2024-06-19 02:43:42 +1000

    create new function in JS

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-const testTextField = document.querySelector('#testTextField');
+const testTextField1 = document.querySelector('#testTextField1');
+const testTextField2 = document.querySelector('#testTextField2');
 
 let clickCounter = 0;
 
-
-document.querySelector('#testTextField').addEventListener('click',
+document.querySelector('#testTextField1').addEventListener('click',
     function() {
-        clickCounter = clickCounter + 1;
-        this.textContent = 'По мне кликнули ' + clickCounter +' раз!';
+        clickCounter = clickCounter + 1; 
+        this.textContent = 'Вы кликнули правильно! ' + clickCounter + ' РАЗ!';
+})
+
+document.querySelector('#testTextField2').addEventListener('click',
+    function() {
+        this.textContent = 'Вы кликнули не правильно!';
 })
